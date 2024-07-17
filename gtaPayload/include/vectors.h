04b5a985c36c5dc0dfb6bf4868c171af572428de
Discord: a0zhar#9539 <96749372+a0zhar@users.noreply.github.com>
2024-07-17 11:11:38 +0200

    Create vectors.h
    NEW! I added custom versions of the following ImGui structs: ImVec2, ImVec3, ImVec4, and ImColor! These new structs can be used just like when using ImGui

---
+++ gtaPayload/include/vectors.h
@@ -0,0 +1,48 @@
+#pragma once
+#ifndef _VECTORS_HH
+#define _VECTORS_HH
+
+struct EpineVec2 {
+    float x, y;
+    constexpr EpineVec2(): x(0.0f), y(0.0f) {}
+    constexpr EpineVec2(float _x, float _y) :
+        x(_x), y(_y) {}
+};
+
+struct EpineVec3 {
+    float x, y, z;
+    constexpr EpineVec3(): x(0.0f), y(0.0f), z(0.0f) {}
+    constexpr EpineVec3(float _x, float _y, float _z) :
+        x(_x), y(_y), z(_z) {}
+};
+
+struct EpineVec4 {
+    float x, y, z, w;
+    constexpr EpineVec4(): x(0.0f), y(0.0f), z(0.0f), w(0.0f) {}
+    constexpr EpineVec4(float _x, float _y, float _z, float _w) :
+        x(_x), y(_y), z(_z), w(_w) {}
+};
+
+struct EpineiRGBA {
+    int r, g, b, a;
+    constexpr EpineiRGBA(): r(0), g(0), b(0), a(255) {}
+    constexpr EpineiRGBA(int _r, int _g, int _b, int _a) :
+        r(_r), g(_g), b(_b), a(_a) {}
+};
+
+struct EpineColor {
+    EpineVec4 Value;
+    inline operator EpineVec4() const { return Value; }
+    constexpr EpineColor() {}
+    constexpr EpineColor(float r, float g, float b, float a = 1.0f): Value(r, g, b, a) {}
+    constexpr EpineColor(const EpineVec4 &col): Value(col) {}
+    constexpr EpineColor(int r, int g, int b, int a = 255): Value(
+        (float)r *(1.0f / 255.0f),
+        (float)g *(1.0f / 255.0f),
+        (float)b *(1.0f / 255.0f),
+        (float)a *(1.0f / 255.0f)
+    ) {}
+};
+
+
+#endif
