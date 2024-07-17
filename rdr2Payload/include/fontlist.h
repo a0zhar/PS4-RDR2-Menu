80f3eddff4286467e77d867b49d10e3581634f7b
Discord: a0zhar#9539 <96749372+a0zhar@users.noreply.github.com>
2024-07-17 11:54:03 +0200

    Renamed gtaPayload dir

---
+++ rdr2Payload/include/fontlist.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef _FONT_LIST_HH
+#define _FONT_LIST_HH
+
+const char *fontList[] = {
+	"util",
+	"catalog5",
+	"body1",
+	"body",
+	"Debug_REG",
+	"catalog4",
+	"chalk",
+	"catalog1",
+	"ledger",
+	"title",
+	"wantedPostersGeneric",
+	"gtaCash",
+	"gamername",
+	"handwritten"
+};
+
+#endif
